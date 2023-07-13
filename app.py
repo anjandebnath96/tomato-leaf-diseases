5588c4167ef0a7f567d18b1bd3c6617e1fa8953a
anjandebnath96 <139436595+anjandebnath96@users.noreply.github.com>
2023-07-13 23:43:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from oauth2client.service_account import ServiceAccountCredentials
+from io import BytesIO
 
 @st.cache(allow_output_mutation=True)
 def load_model():
